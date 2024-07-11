3c8eddcedf7a994a87006aa920f91e647c8b8fa8
Jon Sundin <sundinjon@gmail.com>
2024-07-11 06:42:40 -0700

    createUser implemented in concrete UserDao

--- src/main/java/com/revature/repository/SqliteUserDao.java
+++ src/main/java/com/revature/repository/SqliteUserDao.java
@@ -1,9 +1,11 @@
 package com.revature.repository;
 
 import com.revature.entity.User;
+import com.revature.exception.UserSQLException;
 import com.revature.utility.DatabaseConnector;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,11 +17,19 @@ public class SqliteUserDao  implements UserDao {
         String sql = "INSERT INTO users VALUES (?, ?)";
 
         try(Connection conn = DatabaseConnector.createConnection()){
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, accountInfo.getUsername());
+            ps.setString(2, accountInfo.getPassword());
 
+            int result = ps.executeUpdate();
+            if (result == 1) {
+                return accountInfo;
+            }
+
+            throw new UserSQLException("User account could not be created, please try again.");
         } catch (SQLException e) {
             throw new UserSQLException(e.getMessage());
         }
-        return null;
     }
 
     @Override
