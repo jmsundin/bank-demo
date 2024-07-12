e98ca85f3adf32227e3c4145df3cf6c2ab162539
Jon Sundin <sundinjon@gmail.com>
2024-07-11 17:06:57 -0700

    started implementing CheckingAccountController

---
+++ src/main/java/com/revature/controller/CheckingAccountController.java
@@ -0,0 +1,59 @@
+package com.revature.controller;
+
+import com.revature.service.UserService;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class CheckingAccountController {
+
+    private Scanner scanner;
+    private Map<String, String> controlMap;
+
+    public CheckingAccountController(Scanner scanner, Map<String, String> controlMap) {
+        this.scanner = scanner;
+        this.controlMap = controlMap;
+    }
+
+    public void showMenu() {
+        System.out.println("Accounts (type 1 + ENTER)");
+        System.out.println("New Account (type 2 + ENTER)");
+        System.out.println("Logout (type 3 + ENTER");
+        System.out.println("Quit (type q + ENTER)");
+
+        try {
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "1":
+                    getAccounts();
+                    break;
+                case "2":
+                    createAccount();
+                    break;
+                case "3":
+                    logout();
+                    break;
+                case "q":
+                    // end user session
+                    controlMap.put("continue service", "false");
+            }
+        } catch(Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    public void getAccounts() {
+
+    }
+
+    public void createAccount() {
+
+    }
+
+    public void logout() {
+        System.out.println("Goodbye, " + controlMap.get("user"));
+        controlMap.put("continue service", "false");
+        controlMap.put("user", "");
+    }
+}
