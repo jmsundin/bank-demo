53c4d47782b7856c7dd4c358ce3a44cee72f84ad
Jon Sundin <sundinjon@gmail.com>
2024-07-15 09:42:46 -0700

    replaced controlMap with AppState enum control structure

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -25,12 +25,37 @@ public class Main {
             CheckingAccountService checkingAccountService = new CheckingAccountService(checkingAccountDao);
 
             Map<String, String> controlMap = new HashMap<>();
-            controlMap.put("continue service", "true");
 
             UserController controller = new UserController(scanner, userService, controlMap);
+            CheckingAccountController checkingAccountController = new CheckingAccountController(scanner,
+                    checkingAccountService, controlMap);
 
-            while(Boolean.parseBoolean(controlMap.get("continue service"))) {
-                controller.userPrompt();
+            AppState appState = AppState.LANDING_PROMPT;
+
+            while (appState != AppState.QUIT) {
+                switch (appState) {
+                    case LANDING_PROMPT:
+                        appState = controller.landingPrompt();
+                        break;
+                    case LOGIN:
+                        appState = controller.login();
+                        break;
+                    case REGISTER:
+                        appState = controller.registerUser();
+                        break;
+                    case MAIN_MENU:
+                        appState = checkingAccountController.showMainMenu();
+                        break;
+                    case ACCOUNTS_LIST:
+                        appState = checkingAccountController.getAccounts();
+                        break;
+                    case ACCOUNT_MENU:
+                        appState = checkingAccountController.showAccountOptions();
+                        break;
+                    default:
+                        // appState = controller.userPrompt();
+                        break;
+                }
             }
         }
     }
