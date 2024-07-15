ac910f4ba6cd5f38bf35a5657312e87aee69d60d
Jon Sundin <sundinjon@gmail.com>
2024-07-15 09:40:46 -0700

    removed intelliJ IDEAs tip comments

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -13,12 +13,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        
         try (Scanner scanner = new Scanner(System.in)) {
             UserDao userDao = new SqliteUserDao();
             UserService userService = new UserService(userDao);
