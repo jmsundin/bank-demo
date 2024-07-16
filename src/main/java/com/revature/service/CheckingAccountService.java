e2638528ac0c6ecd12b83a66014335fdc7dbce38
Jon Sundin <sundinjon@gmail.com>
2024-07-15 17:23:30 -0700

    implemented methods for Checking Account service

--- src/main/java/com/revature/service/CheckingAccountService.java
+++ src/main/java/com/revature/service/CheckingAccountService.java
@@ -7,4 +7,31 @@ import com.revature.repository.CheckingAccountDao;
 import java.util.List;
 
 public class CheckingAccountService {
+
+    private CheckingAccountDao checkingAccountDao;
+
+    public CheckingAccountService(CheckingAccountDao checkingAccountDao) {
+        this.checkingAccountDao = checkingAccountDao;
+    }
+
+    public List<CheckingAccount> getCheckingAccounts(String username) {
+        return checkingAccountDao.getCheckingAccounts(username);
+    }
+
+    public CheckingAccount createCheckingAccount(CheckingAccount checkingAccount) {
+        return checkingAccountDao.createCheckingAccount(checkingAccount);
+    }
+
+    public double getAccountBalance(CheckingAccount checkingAccount) {
+        return checkingAccountDao.getCheckingAccountBalance(checkingAccount);
+    }
+
+    public CheckingAccount deposit(CheckingAccount checkingAccount, double amount) {
+        return checkingAccountDao.deposit(checkingAccount, amount);
+    }
+
+    public CheckingAccount withdraw(CheckingAccount checkingAccount, double amount) {
+        return checkingAccountDao.withdraw(checkingAccount, amount);
+    }
+
 }
