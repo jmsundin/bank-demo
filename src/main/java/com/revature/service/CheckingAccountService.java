96746b3b0deff6df77fc6402678d8235651f0fed
Jon Sundin <sundinjon@gmail.com>
2024-07-15 17:41:37 -0700

    removed unused import

--- src/main/java/com/revature/service/CheckingAccountService.java
+++ src/main/java/com/revature/service/CheckingAccountService.java
@@ -1,6 +1,5 @@
 package com.revature.service;
 
-import com.revature.controller.CheckingAccountController;
 import com.revature.entity.CheckingAccount;
 import com.revature.repository.CheckingAccountDao;
 
