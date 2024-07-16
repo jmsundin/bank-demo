8d953a75543cef158c00fed973d82e1f32afb088
Jon Sundin <sundinjon@gmail.com>
2024-07-15 17:07:53 -0700

    create accounts table with foreign key reference to users username

--- src/main/resources/bank-demo-db-setup-script.sql
+++ src/main/resources/bank-demo-db-setup-script.sql
@@ -6,4 +6,12 @@ CREATE TABLE users (
     password TEXT
 );
 
+CREATE TABLE accounts (
+    account_number INTEGER PRIMARY KEY,
+    balance REAL,
+    owner TEXT,
+    FOREIGN KEY (owner) REFERENCES users (username)
+
+);
+
 INSERT INTO users VALUES ('admin', 'admin');
